dfd007785816c9c405803aa0f1e205f7dac591bc
arnetkc <arnetkc@gmail.com>
2024-08-06 17:28:55 +0530

    new in app

--- countries-app/src/App.jsx
+++ countries-app/src/App.jsx
@@ -1,10 +1,46 @@
+import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 import Main from "./pages/Main";
+import axios from "axios";
 
 function App() {
+  const [country, setCountry] = useState([]);
+
+  async function fetchCountryList() {
+    const url = "https://xcountries-backend.azurewebsites.net/all";
+
+    try {
+      const data = await fetch(url);
+      const json = await data.json();
+      console.log(json[0]);
+      return json;
+    } catch (e) {
+      return console.error("Error fetching data:", e.message);
+    }
+  }
+  useEffect(() => {
+    fetchCountryList().then((res) => {
+      setCountry(res);
+    });
+  }, []);
   return (
-    <div>
-      <Main />
+    <div className={styles.wrapper}>
+      <ul className={styles.wrapper2}>
+        {country?.length > 0 &&
+          country.map((res) => {
+            return (
+              <div className={styles.wrapper3} key={res.name}>
+                <img
+                  src={res.flag}
+                  alt="country-flag"
+                  width={150}
+                  height={150}
+                />
+                <p>{res.name}</p>
+              </div>
+            );
+          })}
+      </ul>
     </div>
   );
 }
